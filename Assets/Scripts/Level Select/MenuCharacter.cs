e28f863bbf26c68aef954fe9d0a73e606e8acb09
asarav <asarav@umich.edu>
2016-03-14 19:35:22 -0400

    Added Sample Dialogue Screen, and Interactive Characters

---
+++ Assets/Scripts/Level Select/MenuCharacter.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class MenuCharacter : MonoBehaviour {
+    GameObject mainCharacter;
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.name == "Main Camera")
+        {
+            SceneManager.LoadScene("Date Menu");
+        }
+    }
+
+}
---
+++ Assets/Scripts/Test Dialogue Screen/ChangeScene.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class ChangeScene : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void sceneChange()
+    {
+        print("HI");
+        SceneManager.LoadScene("Joust");
+    }
+}
