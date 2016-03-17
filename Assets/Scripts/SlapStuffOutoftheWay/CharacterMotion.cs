4499666397aafaf75645fe955a8ee206d7669acc
asarav <asarav@umich.edu>
2016-03-17 17:45:14 -0400

    Added Slapping Minigame

---
+++ Assets/Scripts/SlapStuffOutoftheWay/CharacterMotion.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterMotion : MonoBehaviour {
+    Rigidbody rigid;
+    public bool start = false;
+    // Use this for initialization
+    void Start () {
+        rigid = GetComponentInChildren<Rigidbody>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            start = true;
+        }
+        if (start)
+        {
+            if(this.rigid.velocity.z < 16f)
+            {
+                rigid.AddForce(Vector3.forward * 10f);
+            }
+        }
+    }
+
+
+
+}
