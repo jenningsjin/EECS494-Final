01a10f49b1e52424032420c388c8a3d4009136a3
Ben Toll <tollben@umich.edu>
2016-03-19 14:22:15 -0400

    Started Unclog Toilet Minigame

---
+++ Assets/Scripts/UnclogToilet/PlungerScript.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlungerScript : MonoBehaviour {
+	// Use this for initialization
+	private Vector3 startpos;
+	private int state;
+	private float timer;
+	void Start () {
+		startpos = transform.position;
+		state = 0;
+		timer = 0.0f;
+	}
+		
+	void Update () {
+		MovePlunger ();
+	}
+
+	void MovePlunger() {
+		Vector3 endpos = new Vector3 (transform.position.x, transform.position.y - 0.1f, transform.position.z);
+		switch (state) {
+		case (0):
+			if (Input.GetMouseButtonDown (0)) {
+				Debug.Log ("Moving plunger down --> state 1");
+				++state;
+			}
+			break;
+		case (1):
+			transform.position = Vector3.Lerp (startpos, endpos, timer);
+			timer += Time.deltaTime;
+			if (transform.position == endpos) {
+				Debug.Log ("Moving plunger up --> state 2");
+				timer = 0.0f;
+				++state;
+			}
+			break;
+		case (2):
+			transform.position = Vector3.Lerp (endpos, startpos, timer);
+			timer += Time.deltaTime;
+			if (transform.position == startpos) {
+				Debug.Log ("Returning to start --> state 0");
+				timer = 0.0f;
+				state = 0;
+			}
+			break;
+		default:
+			Debug.Log ("Undefined state");
+			break;
+		}
+	}
+}
---
+++ Assets/Scripts/UnclogToilet/ToiletScript.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ToiletScript : MonoBehaviour {
+	public GameObject mainCamera;
+	public GameObject toiletCamera;
+	public GameObject plunger;
+
+	// Use this for initialization
+	void Start () {
+		mainCamera.GetComponent<Camera> ().enabled = true;
+		mainCamera.GetComponent<AudioListener> ().enabled = true;
+		toiletCamera.GetComponent<Camera> ().enabled = false;
+		toiletCamera.GetComponent<AudioListener> ().enabled = false;
+		plunger.SetActive (false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider c)
+	{
+		if (c.gameObject.CompareTag("Player")) {
+			Debug.Log("This is a player");
+			mainCamera.GetComponent<Camera> ().enabled = false;
+			mainCamera.GetComponent<AudioListener> ().enabled = false;
+			toiletCamera.GetComponent<Camera> ().enabled = true;
+			toiletCamera.GetComponent<AudioListener> ().enabled = true;
+			c.gameObject.SetActive (false);
+			plunger.SetActive (true);
+		}
+	}
+}
