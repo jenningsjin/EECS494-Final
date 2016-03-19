9526c1bb1e0b9772a230013cede424469de0c403
jenningsjin <jinjenni@umich.edu>
2016-03-18 21:33:22 -0400

    some more RPS

--- Assets/Scripts/RPS/Options.cs
+++ Assets/Scripts/RPS/Options.cs
@@ -2,6 +2,9 @@
 using System.Collections;
 
 public class Options : MonoBehaviour {
+	enum Moves {Paper, Rock, Scissors};
+	GameObject PlayerMove;
+	GameObject EnemyMove;
 
 	// Use this for initialization
 	void Start () {
@@ -11,5 +14,32 @@ public class Options : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 	
+	}
+
+
+	public void getPlayerMove ( int Move ) {
+		if (Move == (int)Moves.Paper) {
+			print("Paper");
+		}
+
+		else if (Move == (int)Moves.Rock) {
+			print("Rock");
+		}
+
+		else if (Move == (int)Moves.Scissors) {
+			print("Scissors");
+		}
+
+		else {
+			print("Nothing selected");
+		}
+
+		PlayerMove = GameObject.FindWithTag("Player");
+		EnemyMove = GameObject.FindWithTag("Enemy");
+
+
+//		Vector2 transformVector = Vector2(2f, 0f);
+		PlayerMove.transform.Translate(Time.deltaTime * Vector2.up);
+		//print(PlayerMove.name);
 	}
 }
