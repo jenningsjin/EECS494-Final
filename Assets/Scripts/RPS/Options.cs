b35c2680894b2e0a1830e93b406b0978f227120e
jenningsjin <jinjenni@umich.edu>
2016-03-21 14:35:40 -0400

    added animation folders, redid the RPS with GUI Elements

--- Assets/Scripts/RPS/Options.cs
+++ Assets/Scripts/RPS/Options.cs
@@ -5,6 +5,8 @@ public class Options : MonoBehaviour {
 	enum Moves {Paper, Rock, Scissors};
 	GameObject PlayerMove;
 	GameObject EnemyMove;
+	bool textShouldBeShown = false;
+	string displayMessage = "";
 
 	// Use this for initialization
 	void Start () {
@@ -20,26 +22,47 @@ public class Options : MonoBehaviour {
 	public void getPlayerMove ( int Move ) {
 		if (Move == (int)Moves.Paper) {
 			print("Paper");
+			displayMessage = "You Lost...!";
 		}
 
 		else if (Move == (int)Moves.Rock) {
 			print("Rock");
+			displayMessage = "You won!";
 		}
 
 		else if (Move == (int)Moves.Scissors) {
 			print("Scissors");
+			displayMessage = "You Lost...";
 		}
 
 		else {
 			print("Nothing selected");
 		}
 
+		/*
 		PlayerMove = GameObject.FindWithTag("Player");
 		EnemyMove = GameObject.FindWithTag("Enemy");
 
 
-//		Vector2 transformVector = Vector2(2f, 0f);
-		PlayerMove.transform.Translate(Time.deltaTime * Vector2.up);
+		Vector2 transformVector = new Vector2(-20.0f, 0.0f);
+		//print(Vector2.up);
+		PlayerMove.transform.Translate(Time.deltaTime * transformVector, 0);
+		*/
+
 		//print(PlayerMove.name);
+		textShouldBeShown = true;
 	}
+
+
+	void OnGUI() {
+	    if (textShouldBeShown) {
+	        GUI.Label(new Rect(10f, 10f, 100f, 50f), displayMessage  );
+	    }
+	}
+
+
+
 }
+
+
+
