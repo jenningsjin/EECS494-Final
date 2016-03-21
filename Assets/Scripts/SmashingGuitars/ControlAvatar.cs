e30e1a51037ff33aa70f90b813e66b0d18a0b01c
asarav <asarav@umich.edu>
2016-03-21 19:02:51 -0400

    Implement Basic Mechanics for SmashingGuitars Game

--- Assets/Scripts/SmashingGuitars/ControlAvatar.cs
+++ Assets/Scripts/SmashingGuitars/ControlAvatar.cs
@@ -1,15 +1,79 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class ControlNewBehaviourScript : MonoBehaviour {
+public class ControlAvatar : MonoBehaviour {
+    Rigidbody rigid;
+    public GameObject zone1;
+    public GameObject zone2;
+    public GameObject zone3;
+    public GameObject zone4;
+    public GameObject pile;
+    public GameObject guitar;
+    public static int collected = 0;
+    int currentZone = 0;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+    // Use this for initialization
+    void Start () {
+        rigid = GetComponentInChildren<Rigidbody>();
+        zone1.SetActive(false);
+        zone2.SetActive(false);
+        zone3.SetActive(false);
+        zone4.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            rigid.AddForce(Vector3.left * 10f);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            rigid.AddForce(Vector3.right * 10f);
+        }
+        if(collected == 1)
+        {
+            //Randomly pick a zone.
+            currentZone = (int) Random.Range(1f, 4.9f);
+            if(currentZone == 1)
+            {
+                zone1.SetActive(true);
+            } else if(currentZone == 2)
+            {
+                zone2.SetActive(true);
+            } else if(currentZone == 3)
+            {
+                zone3.SetActive(true);
+            } else if(currentZone == 4)
+            {
+                zone4.SetActive(true);
+            }
+            collected = 2;
+        }
+        if(collected == 3)
+        {
+            //Smash Guitar
+            collected = 4;
+        }
+        if(collected == 4)
+        {
+            if (currentZone == 1)
+            {
+                zone1.SetActive(false);
+            }
+            else if (currentZone == 2)
+            {
+                zone2.SetActive(false);
+            }
+            else if (currentZone == 3)
+            {
+                zone3.SetActive(false);
+            }
+            else if (currentZone == 4)
+            {
+                zone4.SetActive(false);
+            }
+            collected = 0;
+        }
 	}
 }
--- Assets/Scripts/SmashingGuitars/ZoneBehavior.cs
+++ Assets/Scripts/SmashingGuitars/ZoneBehavior.cs
@@ -2,7 +2,7 @@
 using System.Collections;
 
 public class ZoneBehavior : MonoBehaviour {
-
+    public bool isZone = true;
 	// Use this for initialization
 	void Start () {
 	
@@ -12,4 +12,17 @@ public class ZoneBehavior : MonoBehaviour {
 	void Update () {
 	
 	}
+
+    void OnTriggerEnter(Collider collision)
+    {
+        if (!isZone)
+        {
+            print("Collected");
+            ControlAvatar.collected = 1;
+        } else if(ControlAvatar.collected == 2 && isZone)
+        {
+            ControlAvatar.collected = 3;
+        }
+        print(collision.gameObject.name);
+    }
 }
